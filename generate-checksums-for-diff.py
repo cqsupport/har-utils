0e0dad0b0c523d54af9d132bfb832391d06efe2c
Andrew Khoury <akhoury@Andrews-MacBook-Pro-2.local>
2019-03-29 13:33:41 -0700

    Adding generate-checksums-for-diff.py python utility script for diffs

---
+++ generate-checksums-for-diff.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys  
+import json
+import re
+import md5
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+if len(sys.argv) < 2 :
+    print ""
+    print "Specify the path of the har file you want to read as a parameter"
+    print "e.g. script.py path/to/harfile.har"
+    print ""
+    exit(1)
+
+
+with open(sys.argv[1]) as fh:
+    a = json.load(fh)
+    for entry in a['log']['entries']:
+        try:
+            url = re.sub(r"(ch)?_(ck|ch)?=[0-9]+&?", r"", entry['request']['url'])
+            url = re.sub(r"\?$", r"", url)
+            url = re.sub(r"^.*://.*?\/", r"/", url)
+        except:
+            pass
+        m = md5.new()
+        m.update(entry['response']['content']['text'])
+        thehash = m.hexdigest()
+        print "%s\t%s" % (url, thehash)
